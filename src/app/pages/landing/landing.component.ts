cc850e6fb1005ef7d7085fd91fac6e7a9e6329e1
jamilahhandini <jamilahhandini@ist.id>
2025-11-26 23:10:20 +0700

    Delete image local for slider

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  showLoading = true;
+  totalImages = 10;
+  loadedImages = 0;
   constructor(private router: Router) {}
 
   goToRoute(route: string): void {
     this.router.navigate([route]);
+  }
+
+  onImageLoad(): void {
+    this.loadedImages++;
+
+    if (this.loadedImages >= this.totalImages) {
+      this.showLoading = false;
+    }
   }
 }
